879727741db3e58f2bffbf5cf204114fb63f40bf
Remy Dufay <remy.dufay@gmail.com>
2022-10-23 19:34:15 +0200

    Adaptative

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#index"
+  root "pages#about"
   get '/association', to: 'pages#about'
   get '/team', to: 'pages#team'
   get '/contact', to: 'pages#contact'
