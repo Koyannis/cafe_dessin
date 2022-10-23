54442768295cf5c2843e1cd211773eb2cedc5d9e
Remy Dufay <remy.dufay@gmail.com>
2022-10-23 18:59:33 +0200

    Base

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#index"
+  get '/association', to: 'pages#about'
+  get '/team', to: 'pages#team'
+  get '/contact', to: 'pages#contact'
+
 end
